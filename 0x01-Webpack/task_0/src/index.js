16325f3eec039713df09e330f4d7af525ac3d9f2
Carlos Soria <soriac367@gmail.com>
2023-05-07 10:01:55 -0500

    Webpack

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+$("body").append(
+  $("<p></p>").text("Holberton Dashboard"),
+  $("<p></p>").text("Dashboard data for the students"),
+  $("<p></p>").text("Copyright - Holberton School")
+);
